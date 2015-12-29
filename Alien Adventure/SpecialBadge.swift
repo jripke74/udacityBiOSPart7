aa41d09eba810ce61349c04c1263a0d9149e29c2
Jeff Ripke <jripke74@gmail.com>
2015-12-28 18:44:39 -0800

    Special Badge: Make it Teal and Defined Animation Types

--- Alien Adventure/SpecialBadge.swift
+++ Alien Adventure/SpecialBadge.swift
@@ -9,5 +9,19 @@
 import SpriteKit
 
 class SpecialBadge: Badge {
+    override init(requestType: UDRequestType) {
+        super.init(requestType: requestType)
+        self.texture = SKTexture(imageNamed: "BadgeTeal")
+        SKSpriteNode.init(texture: texture, color: UIColor.clearColor(), size: CGSizeMake(48, 48))
+    }
 
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    enum BadgeAnimation: Int {
+        case GrowAndShrink = 0
+        case Rotate = 1
+        case Shake = 2
+    }
 }
